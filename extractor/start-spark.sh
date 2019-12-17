afcf62e34af3663f44293db72b9004811739eddf
osharnikov <oleg.sharnikov@kohls.com>
2019-12-17 09:24:47 +0300

    Cron was added

--- extractor/start-spark.sh
+++ extractor/start-spark.sh
@@ -1,4 +1,4 @@
-./spark-2.4.4-bin-hadoop2.7/bin/spark-submit \
+/spark-2.4.4-bin-hadoop2.7/bin/spark-submit \
   --class test.option.iq.Extractor \
   --master local[*] \
-  Extractor.jar
+  /Extractor.jar
